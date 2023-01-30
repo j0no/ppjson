05c48b00141ff568a9d02a0570a0287c9a88c8a6
j0no <jmarcelsw@gmail.com>
2023-01-30 00:31:26 -0800

    Saving progress

--- src/main.rs
+++ src/main.rs
@@ -1,190 +1,280 @@
-use clap::{value_parser, arg, command};
-use serde_json::{from_str, to_string_pretty, Value };
+use clap::{arg, command, value_parser};
+use flatten_json_object::ArrayFormatting;
+use flatten_json_object::Flattener;
+use serde_json::Map;
+use serde_json::Value::{Array, Bool, Null, Number, Object, String as JSONString};
+use serde_json::{from_str, to_string_pretty, Value};
+use std::env;
 use std::env::current_dir;
 use std::fs::read_to_string;
 use std::path::PathBuf;
-use flatten_json_object::ArrayFormatting;
-use flatten_json_object::Flattener;
-use serde_json::Value::{Object, Null, Bool, Number, Array, String as JSONString};
-use std::env;
-use tabled::{Tabled, Table, Style};
-
-
+use tabled::{Style, Table, Tabled};
 
 #[derive(Tabled, Debug)]
 struct JsonTable {
     key: String,
     value: String,
-    value_type: String
+    value_type: String,
 }
 
 fn main() {
     let mut cmd = command!()
         .arg(arg!([INPUT_JSON_FILE] "a json file").value_parser(value_parser!(PathBuf)))
-        .arg(arg!([KEY] "key").value_parser(value_parser!(String)).required(false))
+        .arg(
+            arg!([KEY] "key")
+                .value_parser(value_parser!(String))
+                .required(false),
+        )
         .arg(arg!(table: -t "format as table").value_parser(value_parser!(bool)))
-        .arg(arg!(print_keys: -k "print keys").value_parser(value_parser!(bool)));
+        .arg(arg!(print_keys: -k "print keys").value_parser(value_parser!(bool)))
+        .arg(
+            arg!(key_delimiter: -d [DELIMETER] "set key delimeter")
+                .value_parser(value_parser!(String)),
+        );
+
     let matches = cmd.get_matches_mut();
-    
-    // TODO: Load params from env DEFAULT_JSON_FILE_NAME, DEFAULT_OBJECT_KEY
 
-    // TODO: Read from params from command to get flattened keys or style   
+    // TODO: Read from params from command to get flattened keys or style
 
-    let input = matches
-    .get_one::<PathBuf>("INPUT_JSON_FILE");
-   
-    let path_to_file : Option<PathBuf> = match input {
+    let input = matches.get_one::<PathBuf>("INPUT_JSON_FILE");
+
+    let path_to_file: Option<PathBuf> = match input {
         Some(file_path) => {
             if file_path.is_file() {
-               Some(file_path.to_owned())
+                Some(file_path.to_owned())
             } else {
                 let mut pwd = current_dir().unwrap();
                 pwd.push(file_path);
                 Some(pwd)
             }
-        }  
-        None => None
+        }
+        None => None,
     };
 
-    let key_str = matches
-    .get_one::<String>("KEY");
+    let key_str = matches.get_one::<String>("KEY");
 
-    let print_keys = matches.contains_id("print_keys"); 
+    let key_delimiter = matches.get_one::<String>("key_delimiter");
+
+    let keys_arr = match key_str {
+        Some(key_str_val) => match key_delimiter {
+            Some(key_delimiter_val) => Some(key_str_val.split(key_delimiter_val)),
+            None => None,
+        },
+        None => None,
+    };
+
+    let print_keys = matches.contains_id("print_keys");
     let format_as_tables = matches.contains_id("table");
 
     // get json file content
-    let contents = read_to_string(path_to_file.expect("File not specified")).expect("File doesn't exists");
+    let contents =
+        read_to_string(path_to_file.expect("File not specified")).expect("File doesn't exists");
     let json: Value = from_str(&contents).expect("JSON was not well-formatted");
 
-
     if format_as_tables {
-        let rows = match key_str {
-            Some(key_name) => {
-                get_rows(&json[key_name])
-            }, 
+        match keys_arr {
+            Some(keys_arr_val) => {
+                let mut init = false;
+                let mut past_val: &Value = &Null;
+                for val in keys_arr_val.into_iter() {
+                    if !init {
+                        past_val = &json[val];
+                        init = true;
+                    } else {
+                        past_val = &past_val[val];
+                    }
+                }
+
+                let rows = get_rows(past_val);
+
+                let mut table = Table::new(rows);
+                table.with(Style::modern());
+                println!("{}", table.to_string())
+            }
             None => {
-                get_rows(&(json))
+                let rows = match key_str {
+                    Some(key_name) => get_rows(&json[key_name]),
+                    None => get_rows(&(json)),
+                };
+                let mut table = Table::new(rows);
+                table.with(Style::modern());
+                println!("{}", table.to_string())
             }
         };
-        let mut table = Table::new(rows); 
-        table.with(Style::modern());
-        println!("{}", table.to_string())
-        
     } else {
-        if print_keys {            
+        if print_keys {
             match key_str {
                 Some(key_name) => {
-                    let flattened =  if key_name.eq(&".".to_string()) {
+                    
+                    let flattened = if key_name.eq(&".".to_string()) {
                         Flattener::new()
-                        .set_key_separator(".")
-                        .set_array_formatting(ArrayFormatting::Surrounded {
-                            start: "[".to_string(),
-                            end: "]".to_string()
-                        })
-                        .set_preserve_empty_arrays(false)
-                        .set_preserve_empty_objects(false)
-                        .flatten(&json).unwrap()
+                            .set_key_separator(".")
+                            .set_array_formatting(ArrayFormatting::Surrounded {
+                                start: "[".to_string(),
+                                end: "]".to_string(),
+                            })
+                            .set_preserve_empty_arrays(false)
+                            .set_preserve_empty_objects(false)
+                            .flatten(&json)
+                            .unwrap()
                     } else {
                         Flattener::new()
-                        .set_key_separator(".")
-                        .set_array_formatting(ArrayFormatting::Surrounded {
-                            start: "[".to_string(),
-                            end: "]".to_string()
-                        })
-                        .set_preserve_empty_arrays(false)
-                        .set_preserve_empty_objects(false)
-                        .flatten(&json[key_name]).unwrap()
+                            .set_key_separator(".")
+                            .set_array_formatting(ArrayFormatting::Surrounded {
+                                start: "[".to_string(),
+                                end: "]".to_string(),
+                            })
+                            .set_preserve_empty_arrays(false)
+                            .set_preserve_empty_objects(false)
+                            .flatten(&json[key_name])
+                            .unwrap()
                     };
-                   
 
                     match flattened {
-                        Object(flt_obj) =>{ 
+                        Object(flt_obj) => {
                             let obj_keys = flt_obj.keys().collect::<Vec<_>>();
                             for key_name in obj_keys {
                                 println!("\t{}", key_name);
                             }
-                         },
-                        Null =>{ println!("No keys"); },
-                        Bool(_) =>{ println!("No keys"); },
-                        Number(_) =>{ println!("No keys"); },
-                        Array(_) =>{ println!("No keys"); },
-                        JSONString(_) =>{ println!("No keys"); }
+                        }
+                        Null => {
+                            println!("No keys");
+                        }
+                        Bool(_) => {
+                            println!("No keys");
+                        }
+                        Number(_) => {
+                            println!("No keys");
+                        }
+                        Array(_) => {
+                            println!("No keys");
+                        }
+                        JSONString(_) => {
+                            println!("No keys");
+                        }
                     }
-                },
+                }
                 None => {
                     let flattened = Flattener::new()
                         .set_key_separator(".")
                         .set_array_formatting(ArrayFormatting::Surrounded {
                             start: "[".to_string(),
-                            end: "]".to_string()
+                            end: "]".to_string(),
                         })
                         .set_preserve_empty_arrays(false)
                         .set_preserve_empty_objects(false)
-                        .flatten(&json).unwrap();
-  
+                        .flatten(&json)
+                        .unwrap();
+
                     match flattened {
-                        Object(flt_obj) =>{ 
+                        Object(flt_obj) => {
                             let obj_keys = flt_obj.keys().collect::<Vec<_>>();
                             for key_name in obj_keys {
                                 println!("\t{}", key_name);
                             }
-                         },
-                        Null =>{ println!("No keys"); },
-                        Bool(_) =>{ println!("No keys"); },
-                        Number(_) =>{ println!("No keys"); },
-                        Array(_) =>{ println!("No keys"); },
-                        JSONString(_) =>{ println!("No keys"); }
+                        }
+                        Null => {
+                            println!("No keys");
+                        }
+                        Bool(_) => {
+                            println!("No keys");
+                        }
+                        Number(_) => {
+                            println!("No keys");
+                        }
+                        Array(_) => {
+                            println!("No keys");
+                        }
+                        JSONString(_) => {
+                            println!("No keys");
+                        }
                     }
                 }
             };
         } else {
-            match key_str {
-                Some(key_name) => {
-                    let formatted_json: String = format!("{{ \"{}\": {} }}", key_name, json[key_name].to_string());
-                    let formatted_json_parsed: Value =
-                        from_str(&formatted_json).expect("JSON was not well-formatted");
-                    let pretty_str = to_string_pretty(&formatted_json_parsed).expect("failed");
+            match keys_arr {
+                Some(keys_arr_val) => {
+                    let keys_arr_vec: Vec<&str> = keys_arr_val.collect();
+                    let mut init = false;
+                    let mut past_val: &Value = &Null;
+                    for val in keys_arr_vec.clone().into_iter() {
+                        if !init {
+                            past_val = &json[val];
+                            init = true;
+                        } else {
+                            past_val = &past_val[val];
+                        }
+                    }
+                    
+                    let mut inserted_main_content = false; 
+                    
+                    let mut final_map: Map<String, Value> = Map::new();
+                    for val in keys_arr_vec.into_iter().rev() {
+                        if inserted_main_content {
+                            let mut wrap_map = Map::new();
+                            wrap_map.insert(val.to_string(), final_map.clone().into());
+                            final_map = wrap_map;
+                        } else {
+                            let mut main_map = Map::new();
+                            main_map.insert(val.to_string(), past_val.clone());
+                            final_map = main_map;
+                            inserted_main_content = true;
+                        }
+                        
+                    }
+
+                    let custom_json: Value = final_map.into();
+                    let pretty_str = to_string_pretty(&custom_json).expect("failed");
                     // make new json object
-                    print!("{}", pretty_str);   
-                },
+                    print!("{}", pretty_str);
+                }
                 None => {
                     print!("{}", to_string_pretty(&json).expect("failed"));
                 }
             };
-
         }
     }
-    
-
 }
 
 fn get_rows(json: &Value) -> Vec<JsonTable> {
-    let mut rows : Vec<JsonTable> = Vec::new();
-    
-    match &json {
-            Object(flt_obj) =>{ 
-                // let obj_keys = flt_obj.keys().collect::<Vec<_>>();
-                for (key, value) in flt_obj.into_iter() {
-                   let value_type =  match &value {
-                        Object(_flt_obj) =>{ 
-                           "Object"
-                        },
-                        Null =>{ "Null" },
-                        Bool(_) =>{ "Bool" },
-                        Number(_) =>{ "Number" },
-                        Array(_) =>{ "Array" },
-                        JSONString(_) =>{ "String" }
-                    };
+    let mut rows: Vec<JsonTable> = Vec::new();
 
-                    let pretty_str = to_string_pretty(&value).expect("failed");
-                    rows.push(JsonTable { key: key.to_string(), value: pretty_str.to_string(), value_type: value_type.to_string() });
-                }
-             },
-            Null =>{ println!("No keys"); },
-            Bool(_) =>{ println!("No keys"); },
-            Number(_) =>{ println!("No keys"); },
-            Array(_) =>{ println!("No keys"); },
-            JSONString(_) =>{ println!("No keys"); }
+    match &json {
+        Object(flt_obj) => {
+            // let obj_keys = flt_obj.keys().collect::<Vec<_>>();
+            for (key, value) in flt_obj.into_iter() {
+                let value_type = match &value {
+                    Object(_flt_obj) => "Object",
+                    Null => "Null",
+                    Bool(_) => "Bool",
+                    Number(_) => "Number",
+                    Array(_) => "Array",
+                    JSONString(_) => "String",
+                };
+
+                let pretty_str = to_string_pretty(&value).expect("failed");
+                rows.push(JsonTable {
+                    key: key.to_string(),
+                    value: pretty_str.to_string(),
+                    value_type: value_type.to_string(),
+                });
+            }
         }
+        Null => {
+            println!("No keys");
+        }
+        Bool(_) => {
+            println!("No keys");
+        }
+        Number(_) => {
+            println!("No keys");
+        }
+        Array(_) => {
+            println!("No keys");
+        }
+        JSONString(_) => {
+            println!("No keys");
+        }
+    }
     rows
 }
